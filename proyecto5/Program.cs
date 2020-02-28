8f0f587c3485a73a9620849fecdf2936d85a43d6
victor2k151 <61597715+victor2k151@users.noreply.github.com>
2020-02-28 15:04:56 -0400

    proyecto5

---
+++ proyecto5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace proyecto5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                for (int a = 2; a <= 70; a += 2)
+                {
+                    Console.WriteLine(a);
+                }
+
+                Console.ReadLine();
+            }
+        }
+    }
+}

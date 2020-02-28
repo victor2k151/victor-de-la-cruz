25bf2d17203e0c6c2058c0abdd11f3b96e784272
victor2k151 <61597715+victor2k151@users.noreply.github.com>
2020-02-28 14:46:47 -0400

    proyecto3

---
+++ proyecto3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace proyecto3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un dia de la semana");
+            String dia = Console.ReadLine();
+
+            switch (dia.ToLower())
+            {
+
+                case "Lunes":
+                case "Martes":
+                case "Miercoles":
+                case "Jueves":
+                case "Viernes":
+                    Console.WriteLine("El dia que ingreso no es fin de semana");
+                    break;
+                case "Sabado":
+                case "Domingo":
+                    Console.WriteLine("El dia que ingreso si es fin de semana");
+                    break;
+
+                default:
+                    Console.WriteLine("ERROR, ingrese un dia de la semana.");
+                    break;
+            }
+    }
+         }
+
+}
+

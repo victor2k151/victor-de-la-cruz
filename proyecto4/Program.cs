a93e77e9e1f20299b6688f2b42b77236b6b4f932
victor2k151 <61597715+victor2k151@users.noreply.github.com>
2020-02-28 14:53:06 -0400

    proyecto4

---
+++ proyecto4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace proyecto4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int a = 1; a <= 50; a++)
+            {
+                Console.WriteLine(a);
+            }
+        }
+    }
+}

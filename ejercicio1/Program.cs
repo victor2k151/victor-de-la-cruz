0b9fecbee8c9af6c25bad0fef320d7074a06d79d
victor2k151 <61597715+victor2k151@users.noreply.github.com>
2020-02-28 14:23:25 -0400

    ejercicio1

---
+++ ejercicio1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2, n3;
+
+            Console.WriteLine("Ingrese el primer número: ");
+            n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el segundo número: ");
+            n2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el tercer número: ");
+            n3 = int.Parse(Console.ReadLine());
+            int suma = n1 + n2 + n3;
+
+            Console.WriteLine("El resultado es " + suma);
+
+            Console.ReadLine();
+
+            Console.ReadLine();
+        }
+    }
+}

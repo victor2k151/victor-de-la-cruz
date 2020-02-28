9b5d8c35dd5fcc534c274b8d7e829a2eebbce0c3
victor2k151 <61597715+victor2k151@users.noreply.github.com>
2020-02-28 14:34:26 -0400

    proyecto2

---
+++ proyecto2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace proyecto2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese su nombre completo");
+            String nombre = Console.ReadLine();
+
+            Console.WriteLine("Edad");
+            String edad = Console.ReadLine();
+
+            Console.WriteLine("Su nombre es " + nombre + " y tiene " + edad + " años");
+
+            Console.ReadLine();
+
+        }
+    }
+}
